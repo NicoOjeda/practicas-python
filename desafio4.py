8dd75df85b5991163d868a190d27b8568f21a100
Nico ojeda <nico_ojeda86@hotmail.com>
2023-04-15 22:27:05 -0300

    se termina desafio 4

--- desafio4.py
+++ desafio4.py
@@ -5,3 +5,10 @@
 #Elaborar un algoritmo que permita ingresar el número de partidos Ganados, Perdidos y Empatados, por un equipo en el torneo apertura, se debe demostrar su puntaje total. (Teniendo en cuenta que por cada partido ganado obtendrá 3 puntos, empatado 1 punto y perdido 0 puntos.)
 
 
+ganados= int(input("Ingrese la cantidad de partidos ganados: "))
+perdidos= int(input("Ingrese la cantidad de partidos perdidos: "))
+empatados= int(input("Ingrese la cantidad de partidos empatados: "))
+
+
+puntaje_total = (ganados*3)+(perdidos*0)+(empatados*1)
+print('El puntaje total es:', puntaje_total)
