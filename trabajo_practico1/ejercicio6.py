6a562902062a34d612430450c05184b2dde3917f
Nico ojeda <nico_ojeda86@hotmail.com>
2023-04-28 19:30:53 -0300

    se realizan los ejercicios 5 y 6

---
+++ trabajo_practico1/ejercicio6.py
@@ -0,0 +1,6 @@
+precio_total=float(input('¿Cuanto es el total de la cuenta? $'))
+cantidad_comensales= float(input('¿Cuantos comensales hay?'))
+
+pago_comensal= precio_total / cantidad_comensales
+
+print('Cada persona debe pagar: $', pago_comensal)
