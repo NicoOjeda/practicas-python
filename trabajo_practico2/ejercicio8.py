d7ffe4fa1c8fbc298d333e8bbbd275388c9f00bf
Nico ojeda <nico_ojeda86@hotmail.com>
2023-05-18 16:09:04 -0300

    se realiza ejercicio 8 de tp.2

--- trabajo_practico2/ejercicio8.py
+++ trabajo_practico2/ejercicio8.py
@@ -1,4 +1,10 @@
-# 7. Que imprima todos los números entre el 100 y el 199.
+# 8. Pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+edad= int(input('Ingrese su edad: '))
 
-for i in range(100,200):
-    print(i)
+def edad_usuario(edad):
+    if(edad>=18):
+        print('Usted es mayor de edad')
+    else:
+        print('Usted es menor de edad')
+
+edad_usuario(edad)
