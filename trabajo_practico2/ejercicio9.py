ff4c2f77aa417c679d93ce1b64a954f96bb883da
Nico ojeda <nico_ojeda86@hotmail.com>
2023-05-18 16:09:14 -0300

    se realiza ejercicio 9 de tp.2

--- trabajo_practico2/ejercicio9.py
+++ trabajo_practico2/ejercicio9.py
@@ -1,4 +1,15 @@
-# 7. Que imprima todos los números entre el 100 y el 199.
+# 9. Que el usuario ingrese dos números y muestre cuál de los dos es menor. Considerar el caso
+# en que ambos números son iguales.
 
-for i in range(100,200):
-    print(i)
+numero1= float(input('Ingrese un numero: '))
+numero2= float(input('Ingrese otro numero: '))
+
+def comparar(numero1,numero2):
+    if(numero1>numero2):
+        print(f'{numero2} es menor a {numero1}')
+    elif(numero1<numero2):
+        print(f'{numero1} es menor a {numero2}')
+    else:
+        print(f'{numero2} y {numero1} son iguales')
+
+comparar(numero1,numero2)
