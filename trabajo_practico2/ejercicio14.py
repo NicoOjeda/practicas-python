601cecc44ff18fc50cc06293b96eea9b02935844
Nico ojeda <nico_ojeda86@hotmail.com>
2023-05-18 21:19:04 -0300

    se agrega ejercicio14 resuelto

--- trabajo_practico2/ejercicio14.py
+++ trabajo_practico2/ejercicio14.py
@@ -1,4 +1,17 @@
-# 7. Que imprima todos los números entre el 100 y el 199.
+# 14. Que pida un año y que escriba si es bisiesto o no. Les recordamos que los años bisiestos son
+# múltiplos de 4, pero los múltiplos de 100 no lo son, aunque los múltiplos de 400 sí. Algunos
+# ejemplos de posibles respuestas: 2012 es bisiesto, 2010 no es bisiesto, 2000 es bisiesto, 1900
+# no es bisiesto.
 
-for i in range(100,200):
-    print(i)
+año= int(input('ingrese un año para comprobar si es bisiesto: '))
+
+def bisiesto(año):
+    if (año%4 == 0 and año%100!= 0):
+        print(f'{año} es bisiesto')
+    elif(año%400 ==0):
+        print(f'{año} es bisiesto')
+    else:
+        print(f'{año} no es bisiesto')
+
+bisiesto(año)
+
