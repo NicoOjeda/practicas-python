a63c09751d9d7e23f206acde5e6831e8e829ceb2
Nico ojeda <nico_ojeda86@hotmail.com>
2023-05-19 00:35:00 -0300

    se finaliza ejercicio 15

--- trabajo_practico2/ejercicio15.py
+++ trabajo_practico2/ejercicio15.py
@@ -1,4 +1,26 @@
-# 7. Que imprima todos los números entre el 100 y el 199.
+# 15. Que solicite al usuario una letra y, si es una vocal, muestre el mensaje “es vocal”. Se debe
+# validar que el usuario ingrese sólo un carácter. Si ingresa un string de más de un carácter,
+# informarle que no se puede procesar el dato.
 
-for i in range(100,200):
-    print(i)
+letra = input('Ingrese una letra: ').lower()
+
+def vocal(letra):
+    if (len(letra) == 1):
+            match letra:
+                case 'a':
+                    print(f'{letra} es vocal')
+                case 'e':
+                    print(f'{letra} es vocal')
+                case 'i':
+                    print(f'{letra} es vocal')
+                case 'o':
+                    print(f'{letra} es vocal')
+                case 'u':
+                    print(f'{letra} es vocal')
+                case _:
+                    print(f'{letra} no es vocal')
+    else:
+        print('Ingresaste mas de un caracter, no se puede procesar dato')
+
+vocal(letra)
+
