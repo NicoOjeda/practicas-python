d4d2879df10d40b6d5816169e9c1639230627a7c
Nico ojeda <nico_ojeda86@hotmail.com>
2023-05-18 21:18:53 -0300

    se agrega ejercicio13 resuelto

--- trabajo_practico2/ejercicio13.py
+++ trabajo_practico2/ejercicio13.py
@@ -1,4 +1,17 @@
-# 7. Que imprima todos los números entre el 100 y el 199.
+# 13. Que resuelva el siguiente problema: los alumnos de un curso se han dividido en dos grupos
+# A y B de acuerdo al sexo y el nombre. El grupo A está formado por las mujeres con un
+# nombre anterior a la M y los hombres con un nombre posterior a la N y el grupo B por el
+# resto. Escribir un programa que pregunte al usuario su nombre y sexo y muestre por pantalla
+# el grupo que le corresponde.
 
-for i in range(100,200):
-    print(i)
+nombre = input('Ingrese su nombre: ').lower()
+inicial= nombre[0]
+sexo = input('Ingrese la letra de su sexo Femenino "f" o masculino "m": ').lower()
+
+def grupos(inicial,sexo):
+    if((inicial< "m" and sexo=="f") or (inicial> "n" and sexo=="m")):
+        print('Tu grupo es el A')
+    else:
+        print('Tu grupo es el B')
+        
+grupos(inicial,sexo)
