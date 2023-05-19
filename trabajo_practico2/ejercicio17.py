8bef88a8a70ad2acd6938a453d4c0e06937a845e
Nico ojeda <nico_ojeda86@hotmail.com>
2023-05-19 20:54:40 -0300

    se finaliza ejercicio 17 de tp2

--- trabajo_practico2/ejercicio17.py
+++ trabajo_practico2/ejercicio17.py
@@ -1,6 +1,19 @@
 # 17. Que muestre todos los números primos entre 0 y 100 e imprima cuántos números primos hay.
 contador = 0
 
-# for i in range(0,101,1):
-#     print(i)
-#     if(i%)
+def numeros_primos():
+    contador2= 0
+    for i in range(1,101,1):
+        contador = 0
+        for c in range(1,i+1,1):
+            if(i%c ==0):
+                contador += 1
+                
+        if(contador==2):
+            print(i)
+            contador2 += 1
+        else:
+            print(i)
+    print(f'Entre 0 y 100 hay {contador2} numeros primos')
+        
+numeros_primos()
