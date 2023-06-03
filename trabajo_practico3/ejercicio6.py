fd09b1dbcd540714b0a7f5da58736644b77ea690
Nico ojeda <nico_ojeda86@hotmail.com>
2023-06-02 23:01:09 -0300

    se termina ejercicio6

--- trabajo_practico3/ejercicio6.py
+++ trabajo_practico3/ejercicio6.py
@@ -11,6 +11,7 @@ def elemento_final(agregar_final,lista_vacia):
     print(lista_vacia)
 
 elemento_final(agregar_final,lista_vacia)
+print("----------------")
 
 # b. Agregar un elemento al principio.
 agregar_principio = input('Ingrese un elemento al principio de la lista: ')
@@ -23,4 +24,29 @@ elemento_final(agregar_principio,lista_vacia)
 print("----------------")
 
 # c. Quitar un elemento al final.
+
+quitar= input('Si desea eliminar un elemento al final (si,no):').lower()
+
+def quitar_elemento_final(quitar):
+    if(quitar == "si"):
+        del lista_vacia[-1]
+        print('Borraste el ultimo elemento de la lista')
+    else:
+        print("No borraste nada")
+    print(lista_vacia)
+
+quitar_elemento_final(quitar)
+
 # d. Quitar un elemento al principio.
+
+quitar_principio= input('Si desea eliminar un elemento al principio (si,no):').lower()
+
+def quitar_elemento_final(quitar_principio):
+    if(quitar_principio == "si"):
+        del lista_vacia[0]
+        print('Borraste el ultimo elemento de la lista')
+    else:
+        print("No borraste nada")
+    print(lista_vacia)
+
+quitar_elemento_final(quitar_principio)
